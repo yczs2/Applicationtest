7f1c10e084a004b296c2d18a130f8e53836af0ad
yancheng <2257469137@qq.com>
2024-02-15 14:53:44 +0800

    second time

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Androi1111222d")
+        Greeting("Androi11112266662d")
     }
 }
